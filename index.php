ad3af56ba1c5ef07d1f0ddf7bc7c3620403a090a
Zhenia <jeka153333153@mail.ru>
2017-07-14 14:59:02 +0300

    initial project version 1.1

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<HTML>
+<HEAD>
+    <TITLE>CALC</TITLE>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/bootstrap-theme.css" rel="stylesheet">
+    <link href="css/bootstrap-theme.min.css" rel="stylesheet">
+    <script type="text/javascript" src="{THEME}/js/bootstrap.min.js"></script>
+</HEAD>
+<BODY>
+<script src="http://code.jquery.com/jquery-latest.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<H1>Calc INDEX.PHP</H1>
+<form action="test.php">
+    <p><input name="a" id="a" value="0"></p>
+    <select name = "z" id="operations">
+        <option value = "+">+
+        <option value = "-">-
+        <option value = "*">*
+        <option value = "/">/
+    </select>
+    <p><input name="b" id="b"  value="0"></p>
+    <p><input type="submit" class="btn btn-primary" value="rezz"/> </p>
+</form>
+
+<div id="div1"></div>
+</BODY>
+</HTML>
